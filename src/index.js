8ddd0b93304b0de4f0a2c554c8dcf85adf8e13ca
thehead <shubhamwagh2544@gmail.com>
2024-12-03 09:17:03 +0530

    zip story: in progress

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import moment from "moment";
+import moment from 'moment';
 import multer from 'multer';
 import _ from 'lodash';
 import path from 'path';
 import xlsx from 'xlsx';
+import fs from 'fs';
 
 import User from './User.model.js';
 import Role from './Role.model.js';
-import { connectDatabase } from './dbconfig.js';
+import { connectDatabase, sequelize } from './dbconfig.js';
 import { syncModels } from './associations.js';
 import UserRole from './UserRole.model.js';
 import Post from './Post.model.js';
@@ -32,9 +33,9 @@ setUpDatabase()
   });
 
 const pngFilter = function (req, file, cb) {
-  const filetypes = ['.png'];
-  const mimetypes = ['image/png'];
-  const fileExtensionName = path.extname(file.originalname)
+  const filetypes = ['.png', '.jpg', '.jpeg'];
+  const mimetypes = ['image/png', 'image/jpg', 'image/jpeg'];
+  const fileExtensionName = path.extname(file.originalname);
 
   const extname = _.includes(filetypes, fileExtensionName);
   const mimetype = _.includes(mimetypes, file.mimetype);
@@ -42,28 +43,30 @@ const pngFilter = function (req, file, cb) {
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    return cb(new Error('Only PNG image allowed!'));
+    return cb(new Error('Only PNG/JPEG image allowed!'));
   }
 };
 
 const xlsxFilter = function (req, file, cb) {
-    const filetypes = ['.xlsx'];
-    const mimetypes = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-    const fileExtensionName = path.extname(file.originalname)
+  const filetypes = ['.xlsx'];
+  const mimetypes = [
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  ];
+  const fileExtensionName = path.extname(file.originalname);
 
-    const extname = _.includes(filetypes, fileExtensionName);
-    const mimetype = _.includes(mimetypes, file.mimetype);
+  const extname = _.includes(filetypes, fileExtensionName);
+  const mimetype = _.includes(mimetypes, file.mimetype);
 
-    if (extname && mimetype) {
-        return cb(null, true);
-    } else {
-        return cb(new Error('Only XLSX file allowed!'));
-    }
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    return cb(new Error('Only XLSX file allowed!'));
+  }
 };
 
 // multer middleware
 const upload = multer({
-  fileFilter: xlsxFilter,
+  fileFilter: pngFilter,
   storage,
   limits: {
     fileSize: 5 * 1024 * 1024,
@@ -73,81 +76,220 @@ const upload = multer({
 app.use(express.json());
 app.use(cors());
 
-// bulk create users
-app.post('/api/users/bulk', async (req, res) => {
-    try {
-        const users = req.body;
-        if (!users.length) {
-        return res.status(400).json({ message: 'No users found' });
-        }
-
-        // bulk create users
-        await User.bulkCreate(users);
-        return res.status(201).json({ message: 'Users bulk created successfully' });
-    } catch (error) {
-        console.log('Error importing users', error);
-        return res.status(500).json({ message: 'Error bulk creating users' });
-    }
-});
-
-// bulk import users
-app.post('/api/users/import',
-    (req, res, next) => {
+// upload profile picture for a user
+app.post(
+  '/api/users/:id/profile/upload',
+  (req, res, next) => {
     upload(req, res, (err) => {
       if (err) {
         return res.status(400).json({ message: err.message });
       }
       next();
     });
-    },
-    async (req, res) => {
-  try {
-    const excelFile = req.file;
-    if (!excelFile) {
-      return res.status(400).json({ message: 'No file found' });
-    }
+  },
+  async (req, res) => {
+    try {
+      const file = req.file;
+      if (_.isNil(file)) {
+        return res.status(400).json({ message: 'File is required' });
+      }
+      // check if user exists
+      const id = req.params.id;
+      if (_.isNil(id)) {
+        return res.status(400).json({ message: 'User id is required' });
+      }
+      let user = await User.findOne({
+        where: {
+          id,
+          isActive: true,
+        },
+      });
+      if (_.isNil(user)) {
+        return res.status(404).json({ message: 'User not found' });
+      }
 
-    // bulk import users
-    const workBook = xlsx.read(excelFile.buffer, {type: 'buffer'});   // reading from memory storage (not disk storage) hence read and not readFile
-    const sheetName = workBook.SheetNames[0];
-    const sheet = workBook.Sheets[sheetName];
+      // user found
+      user.profile = file.buffer;
+      user.profileMimeType = file.mimetype; // Save the MIME type
+      await user.save();
 
-    // convert sheet to json
-    // const users = xlsx.utils.sheet_to_json(sheet);
-
-    // data validation
-    const users = xlsx.utils.sheet_to_json(sheet, {header: 1});
-    if (!users.length) {
-      return res.status(400).json({ message: 'Empty File' });
-    }
-
-    const userHeaders = users[0];
-    const userAttributes = Object.keys(_.groupBy(User.getAttributes(), 'field'));
-
-    let errorObject = "";
-    _.each(userAttributes, (attribute) => {
-        if (!_.includes(userHeaders, attribute)) {
-          errorObject += `${attribute}`
-        }
-    });
-    if (errorObject !== "") {
-      return res.status(400).json({ message: `Missing ${errorObject} in the file` });
-    }
-
-    const userData = users.slice(1);
-    const userObjects = [];
-    for (const dataArray of userData) {
-      if (dataArray.length > 0) {
-        // check if user exists in db
-        const user = await User.findOne({
+      // return user without profile buffer
+      user = await User.findOne(
+        {
           where: {
-            id: dataArray[0],
-            isActive: true
-          }
-        });
-        if (user) {
-          if (dataArray[1] !== user.firstname || dataArray[2] !== user.lastname || dataArray[3] !== user.email || dataArray[4] !== user.password || dataArray[7] !== user.isActive || dataArray[8] !== user.createdAt || dataArray[9] !== user.updatedAt) {
-            await user.update({
+            id,
+            isActive: true,
+          },
+        },
+        {
+          attributes: {
+            exclude: ['profile', 'profileMimeType', 'password'],
+          },
+        }
+      );
+      return res
+        .status(200)
+        .json({ message: 'Profile uploaded successfully', user });
+    } catch (error) {
+      console.log('Error uploading profile picture', error);
+      return res
+        .status(500)
+        .json({ message: 'Error uploading profile picture' });
+    }
+  }
+);
+
+app.get('/api/users/:userId/profile/optimal', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    if (_.isNil(userId)) {
+      return res.status(400).json({
+        msg: 'invalid user id',
+      });
+    }
+
+    // get user (profile name saved in another table)
+    const user = await User.findByPk(userId);
+    if (_.isNil(user)) {
+      return res.status(400).json({
+        msg: 'invalid user',
+      });
+    }
+
+    // store user profile in a zip on persistent storage
+    // 1. define directory
+    const profilesDir = path.join(__dirname, 'profiles');
+    // 2. define profile path in dir
+    const profilePath = path.join(profilesDir, `${userId}.jpeg`);
+
+    // 3. check if directory exists
+    if (!fs.existsSync(profilesDir)) {
+      fs.mkdirSync(profilesDir, { recursive: true });
+    }
+
+    // 4.check if file already exists
+    if (!fs.existsSync(profilePath)) {
+      // get user profile
+      const profile = user.dataValues.profile;
+      // write file to storage
+      fs.writeFileSync(profilePath, profile);
+    }
+
+    // 5. serve file
+    return res.sendFile(profilePath);
+
+    // todo: zip ?
+    // todo: cron ?
+    // todo: cache ?
+    // todo: notification ?
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    res.status(500).json({
+      msg: 'Internal server error: Something went wrong',
+    });
+  }
+});
+
+// bulk create users
+app.post('/api/users/bulk', async (req, res) => {
+  try {
+    const users = req.body;
+    if (!users.length) {
+      return res.status(400).json({ message: 'No users found' });
+    }
+
+    // bulk create users
+    await User.bulkCreate(users);
+    return res.status(201).json({ message: 'Users bulk created successfully' });
+  } catch (error) {
+    console.log('Error importing users', error);
+    return res.status(500).json({ message: 'Error bulk creating users' });
+  }
+});
+
+// bulk import users
+app.post(
+  '/api/users/import',
+  (req, res, next) => {
+    upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ message: err.message });
+      }
+      next();
+    });
+  },
+  async (req, res) => {
+    try {
+      const excelFile = req.file;
+      if (!excelFile) {
+        return res.status(400).json({ message: 'No file found' });
+      }
+
+      // bulk import users
+      const workBook = xlsx.read(excelFile.buffer, { type: 'buffer' }); // reading from memory storage (not disk storage) hence read and not readFile
+      const sheetName = workBook.SheetNames[0];
+      const sheet = workBook.Sheets[sheetName];
+
+      // convert sheet to json
+      // const users = xlsx.utils.sheet_to_json(sheet);
+
+      // data validation
+      const users = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+      if (!users.length) {
+        return res.status(400).json({ message: 'Empty File' });
+      }
+
+      const userHeaders = users[0];
+      const userAttributes = Object.keys(
+        _.groupBy(User.getAttributes(), 'field')
+      );
+
+      let errorObject = '';
+      _.each(userAttributes, (attribute) => {
+        if (!_.includes(userHeaders, attribute)) {
+          errorObject += `${attribute}`;
+        }
+      });
+      if (errorObject !== '') {
+        return res
+          .status(400)
+          .json({ message: `Missing ${errorObject} in the file` });
+      }
+
+      const userData = users.slice(1);
+      const userObjects = [];
+      for (const dataArray of userData) {
+        if (dataArray.length > 0) {
+          // check if user exists in db
+          const user = await User.findOne({
+            where: {
+              id: dataArray[0],
+              isActive: true,
+            },
+          });
+          if (user) {
+            if (
+              dataArray[1] !== user.firstname ||
+              dataArray[2] !== user.lastname ||
+              dataArray[3] !== user.email ||
+              dataArray[4] !== user.password ||
+              dataArray[7] !== user.isActive ||
+              dataArray[8] !== user.createdAt ||
+              dataArray[9] !== user.updatedAt
+            ) {
+              await user.update({
+                firstname: dataArray[1],
+                lastname: dataArray[2],
+                email: dataArray[3],
+                password: dataArray[4],
+                isActive: dataArray[7],
+                createdAt: moment(dataArray[8]).format('YYYY-MM-DD'),
+                updatedAt: moment(dataArray[9]).format('YYYY-MM-DD'),
+              });
+            }
+          } else {
+            userObjects.push({
+              id: dataArray[0],
               firstname: dataArray[1],
               lastname: dataArray[2],
               email: dataArray[3],
@@ -155,32 +297,20 @@ app.post('/api/users/import',
               isActive: dataArray[7],
               createdAt: moment(dataArray[8]).format('YYYY-MM-DD'),
               updatedAt: moment(dataArray[9]).format('YYYY-MM-DD'),
-            })
+            });
           }
-        } else {
-          userObjects.push({
-            id: dataArray[0],
-            firstname: dataArray[1],
-            lastname: dataArray[2],
-            email: dataArray[3],
-            password: dataArray[4],
-            isActive: dataArray[7],
-            createdAt: moment(dataArray[8]).format('YYYY-MM-DD'),
-            updatedAt: moment(dataArray[9]).format('YYYY-MM-DD'),
-          });
         }
       }
 
+      // bulk create users
+      userObjects.length ? await User.bulkCreate(userObjects) : null;
+      return res.status(201).json({ message: 'Users imported successfully' });
+    } catch (error) {
+      console.log('Error importing users', error);
+      return res.status(500).json({ message: 'Error importing users' });
     }
-
-    // bulk create users
-    userObjects.length ? await User.bulkCreate(userObjects) : null;
-    return res.status(201).json({ message: 'Users imported successfully' });
-  } catch (error) {
-    console.log('Error importing users', error);
-    return res.status(500).json({ message: 'Error importing users' });
   }
-});
+);
 
 // create a user
 app.post('/api/users', async (req, res) => {
@@ -723,69 +853,6 @@ app.get('/api/profiles/:id/user', async (req, res) => {
   }
 });
 
-// upload profile picture for a user
-app.post(
-  '/api/users/:id/profile/upload',
-    (req, res, next) => {
-      upload(req, res, (err) => {
-        if (err) {
-          return res.status(400).json({ message: err.message });
-        }
-        next();
-      });
-    },
-  async (req, res) => {
-    try {
-      const file = req.file;
-      if (_.isNil(file)) {
-        return res.status(400).json({ message: 'File is required' });
-      }
-      // check if user exists
-      const id = req.params.id;
-      if (_.isNil(id)) {
-        return res.status(400).json({ message: 'User id is required' });
-      }
-      let user = await User.findOne({
-        where: {
-          id,
-          isActive: true,
-        },
-      });
-      if (_.isNil(user)) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      // user found
-      user.profile = file.buffer;
-      user.profileMimeType = file.mimetype; // Save the MIME type
-      await user.save();
-
-      // return user without profile buffer
-      user = await User.findOne(
-        {
-          where: {
-            id,
-            isActive: true,
-          },
-        },
-        {
-          attributes: {
-            exclude: ['profile', 'profileMimeType', 'password'],
-          },
-        }
-      );
-      return res
-        .status(200)
-        .json({ message: 'Profile uploaded successfully', user });
-    } catch (error) {
-      console.log('Error uploading profile picture', error);
-      return res
-        .status(500)
-        .json({ message: 'Error uploading profile picture' });
-    }
-  }
-);
-
 // download user profile
 // TODO ? Not Working
 app.get('/api/users/:id/profile/download', async (req, res) => {
@@ -819,7 +886,7 @@ app.get('/api/users/:id/profile/download', async (req, res) => {
     res.setHeader('Content-Disposition', 'attachment; filename=profile');
     res.setHeader('Content-Type', mimetype);
 
-    res.send(profile);
+    res.sendFile(profile);
   } catch (error) {
     console.log('Error downloading profile picture', error);
     return res
